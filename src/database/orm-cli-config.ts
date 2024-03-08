05518717dbf15ad20ae8c43b26af462c501a4d71
Meiado <gabrielmeiado@hotmail.com>
2024-03-08 16:55:02 -0300

    Created many-to-many table with migrations

--- src/database/orm-cli-config.ts
+++ src/database/orm-cli-config.ts
@@ -2,9 +2,18 @@ import { DataSource } from 'typeorm';
 import { dataSourceOptions } from './database.module';
 import { CreateCoursesTable1709920044288 } from 'src/migrations/1709920044288-CreateCoursesTable';
 import { CreateTagsTable1709920044288 } from 'src/migrations/1709921587841-CreateTagsTable';
+import { CreateCoursesTagsTable1709922494844 } from 'src/migrations/1709922494844-CreateCoursesTagsTable';
+import { AddCoursesIdToCoursesTagsTable1709923101232 } from 'src/migrations/1709923101232-AddCoursesIdToCoursesTagsTable';
+import { AddTagsIdToCoursesTagsTable1709923772484 } from 'src/migrations/1709923772484-AddTagsIdToCoursesTagsTable';
 
 export const dataSource = new DataSource({
   ...dataSourceOptions,
   synchronize: false,
-  migrations: [CreateCoursesTable1709920044288, CreateTagsTable1709920044288],
+  migrations: [
+    CreateCoursesTable1709920044288,
+    CreateTagsTable1709920044288,
+    CreateCoursesTagsTable1709922494844,
+    AddCoursesIdToCoursesTagsTable1709923101232,
+    AddTagsIdToCoursesTagsTable1709923772484,
+  ],
 });
